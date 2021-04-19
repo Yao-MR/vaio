95231fdcfff2d474a55a498a356eddc5aa3b40f3
Yao <yao.mr.cn@gmail.com>
2021-04-19 12:01:18 +0800

    updater

--- service/spring-boot/src/main/java/com/vaio/io/spring/boot/VaioApplication.java
+++ service/spring-boot/src/main/java/com/vaio/io/spring/boot/VaioApplication.java
@@ -1,11 +1,12 @@
 package com.vaio.io.spring.boot;
 
 import com.vaio.io.spring.boot.listener.MyApplicationStartedEventListener;
+import com.vaio.io.spring.boot.repository.JpaUserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VaioApplication {
+public class VaioApplication implements {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(VaioApplication.class);
         app.addListeners(new MyApplicationStartedEventListener());
---
+++ service/spring-boot/src/main/java/com/vaio/io/spring/boot/repository/JpaUserRepository.java
@@ -0,0 +1,9 @@
+package com.vaio.io.spring.boot.repository;
+
+/**
+ * @author yao.wang (vaio.MR.CN@GMail.com)
+ * @date 2021-04-15
+ */
+public interface JpaUserRepository extends {
+
+}
